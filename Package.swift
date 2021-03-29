836d91dc913912a194d757c7b1c553fa89e1628d
Christian Miranti <christian.miranti@gmail.com>
2021-03-29 11:18:41 +0200

    Fix broken spm dependencies

--- Package.swift
+++ Package.swift
@@ -14,8 +14,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-		.package(url: "https://github.com/facile-it/FunctionalKit.git", from: Version(0,0,0)),
-		.package(url: "https://github.com/ReactiveX/RxSwift.git", from: "4.0.0")
+		.package(url: "https://github.com/facile-it/FunctionalKit.git", from: Version(0,23,0)),
+        .package(url: "https://github.com/typelift/Abstract.git", from: Version(0,1,0)),
+        .package(url: "https://gitlab.facile.it/mobile-apps/Log", from: Version(0,5,0)),
+		.package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,6 +26,8 @@ let package = Package(
             name: "NavigationHelper",
             dependencies: [
 				"FunctionalKit",
+                "Abstract",
+                "Log",
 				"RxSwift"
 			]),
         .testTarget(
