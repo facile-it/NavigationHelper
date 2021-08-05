37f95c9bc2449ac9dbefa228bfe4ce47c302fa1b
Andrea <andrea.rinaldi91@gmail.com>
2021-08-05 16:14:04 +0200

    Update RxSwift (#6)
    * Update RxSwift to 6.2.0
    
    * Fix warning in SerialHandlerTests

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "NavigationHelper",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NavigationHelper",
             targets: ["NavigationHelper"]),
@@ -15,24 +14,20 @@ let package = Package(
             targets: ["NavigationHelperUIKit"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-		.package(url: "https://github.com/facile-it/FunctionalKit.git", from: Version(0,23,0)),
+        .package(url: "https://github.com/facile-it/FunctionalKit.git", from: Version(0,23,0)),
         .package(url: "https://github.com/typelift/Abstract.git", from: Version(0,1,0)),
         .package(url: "https://gitlab.facile.it/mobile-apps/Log", from: Version(0,5,0)),
-		.package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: Version(6,0,0))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NavigationHelper",
             dependencies: [
-				"FunctionalKit",
+                "FunctionalKit",
                 "Abstract",
                 "Log",
-				"RxSwift"
-			]),
+                "RxSwift"
+            ]),
         .target(
             name: "NavigationHelperUIKit",
             dependencies: [
